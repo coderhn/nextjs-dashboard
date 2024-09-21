66158638c1fa9ba0bddc070542ab41a566390ece
wangyuhuan <wangyuhuan@yqn.com>
2024-09-21 16:31:10 +0800

    🐞 fix(layout 修复字体导入): 修复字体导入

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/app/ui/global.css";
-import { inter } from "@/app/ui/fonts.ts";
+import { inter } from "@/app/ui/fonts";
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
